375c664cee7d207745b1e8f97e79d59e3168d9c5
Isaac Brito <isaac.cris.brito@gmail.com>
2023-01-20 09:03:56 -0300

    Add Health checks

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 using Catalog.Repositories;
 using Catalog.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using System;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers(options => {
+    //Added by isaac
     options.SuppressAsyncSuffixInActionNames = false;
 });
 
@@ -17,17 +23,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+//added by Isaac
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
+//Added by Isaac - Mongo Configuration
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
     {
-        var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-
-        return new MongoClient(settings.ConnectionString);
+        
+        return new MongoClient(mongoDbSettings.ConnectionString);
     });
 
+
+//Added by Isaac - Mongo Configuration
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
+//Configs of health
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoDbSettings.ConnectionString,
+        name: "mongo",
+        timeout: new TimeSpan(0, 0,3),
+        tags: new[] { "ready" }
+        );
 
 var app = builder.Build();
 
@@ -41,6 +61,31 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+//Isaac add healthchecks
+app.MapHealthChecks("/healthz/ready", new HealthCheckOptions{
+    Predicate       = (check) => check.Tags.Contains("ready"),
+    ResponseWriter  = async(context, report) => {
+        var result = JsonSerializer.Serialize(
+            new{
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new {
+                    name      = entry.Key,
+                    status    = entry.Value.Status.ToString(),
+                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                    duration  = entry.Value.Duration.ToString()
+                })
+            }
+        );
+
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
+app.MapHealthChecks("/healthz/live", new HealthCheckOptions{
+    Predicate = (_) => false
+});
 
 app.MapControllers();
 
